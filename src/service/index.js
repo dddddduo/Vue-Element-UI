b9d1a002daac820672ecfeb19486b01e47e5ad11
leiyunduo <1810856114@qq.com>
2019-10-18 17:45:38 +0800

    接口参数备注

--- src/service/index.js
+++ src/service/index.js
@@ -1,5 +1,8 @@
 import { LYD } from '../config/LYD'
 
 // 例如：
-// export const posts = params => LYD.POST("/def/post/jiekou", params)
-// export const gets = params => LYD.GET("/def/get/jiekou", params)
+// 如果post接口是json格式，需在最后加上'json'
+// export const posts1 = params => MTHR.POST("/def/post/jiekou", params, 'json')
+// export const posts = params => MTHR.POST("/def/post/jiekou", params)
+// 如果get接口无参数，不需要传params
+// export const gets = params => MTHR.GET("/def/get/jiekou", params)
