c66744b72c5b9ed3515c465d79113d00b05ed902
leiyunduo <1810856114@qq.com>
2019-10-22 18:38:06 +0800

    新加日历功能

--- src/pages/home/home_one.vue
+++ src/pages/home/home_one.vue
@@ -1,3 +1,146 @@
 <template>
-  <div id="home_one">home_one</div>
+  <div id="home_one">
+    <el-row :gutter="20">
+      <el-col :xs="1" :sm="1" :md="1" :lg="1" :xl="1">
+        <div class="students-list">
+          <div class="icon-list"><i class="el-icon-arrow-left" @click="leftClick"></i></div>
+        </div>
+      </el-col>
+      <el-col :xs="7" :sm="7" :md="7" :lg="7" :xl="7">
+        <div class="students-list">
+          <Calendar
+            v-on:choseDay="clickDay"
+            v-on:changeMonth="changeDate"
+            :sundayStart="true"
+            :markDateMore='checkList'
+            ref="Calendar"
+            :textTop="['日', '一', '二', '三', '四', '五', '六']"
+          ></Calendar>
+        </div>
+      </el-col>
+      <el-col :xs="7" :sm="7" :md="7" :lg="7" :xl="7">
+        <div class="students-list">
+          <Calendar
+            ref="Calendar1"
+            v-on:choseDay="clickDay"
+            v-on:changeMonth="changeDate"
+            :sundayStart="true"
+            :markDateMore='checkList'
+            :textTop="['日', '一', '二', '三', '四', '五', '六']"
+          ></Calendar>
+        </div>
+      </el-col>
+      <el-col :xs="7" :sm="7" :md="7" :lg="7" :xl="7">
+        <div class="students-list">
+          <Calendar
+            ref="Calendar2"
+            v-on:choseDay="clickDay"
+            v-on:changeMonth="changeDate"
+            :sundayStart="true"
+            :markDateMore='checkList'
+            :textTop="['日', '一', '二', '三', '四', '五', '六']"
+          ></Calendar>
+        </div>
+      </el-col>
+      <el-col :xs="1" :sm="1" :md="1" :lg="1" :xl="1">
+        <div class="students-list">
+          <div class="icon-list"><i class="el-icon-arrow-right" @click="rightClick"></i></div>
+        </div>
+      </el-col>
+    </el-row>
+    <!-- // v-on:isToday="clickToday"
+    // :markDate=arr // arr=['2018/4/1','2018/4/3'] 标记4月1日和4月3日 简单标记
+    //:markDateMore=arr // 多种不同的标记
+    // 第一个标记和第二个标记不能同时使用
+    // :agoDayHide='1514937600' //某个日期以前的不允许点击  时间戳10位
+    // :futureDayHide='1525104000' //某个日期以后的不允许点击  时间戳10位
+    // :sundayStart="true" //默认是周一开始 当是true的时候 是周日开始
+      arr=[{date:'2018/4/1',className:"mark1"}, {date:'2018/4/13',className:"mark2"}] -->
+  </div>
 </template>
+
+<script>
+import Calendar from 'vue-calendar-component'
+export default {
+  data () {
+    return {
+      checkList: [
+        { date: '2019-10-11', className: 'calendar-mark-wh' },
+        { date: '2019-10-15', className: 'calendar-mark-wh' },
+        { date: '2019-10-18', className: 'calendar-mark-wh' }
+      ]
+    }
+  },
+  components: {
+    Calendar
+  },
+  mounted () {
+    let that = this
+    this.$refs.Calendar1.NextMonth('');
+    this.$refs.Calendar2.NextMonth('');
+    this.$refs.Calendar2.NextMonth('');
+    that.checkList[
+      { date: '2019-10-11', className: 'calendar-mark-wh' },
+      { date: '2019-10-15', className: 'calendar-mark-wh' },
+      { date: '2019-10-18', className: 'calendar-mark-wh' }
+    ]
+  },
+  methods: {
+    clickDay(data) {
+      console.log(1)
+      console.log(data); //选中某天
+    },
+    leftClick () {
+      this.$refs.Calendar.PreMonth('');
+      this.$refs.Calendar1.PreMonth('');
+      this.$refs.Calendar2.PreMonth('');
+      // this.$refs.Calendars.ChoseMonth("2018-12-13");
+    },
+    rightClick () {
+      this.$refs.Calendar.NextMonth('');
+      this.$refs.Calendar1.NextMonth('');
+      this.$refs.Calendar2.NextMonth('');
+    },
+    changeDate(data) {
+      console.log(2)
+      console.log(data); //左右点击切换月份
+    },
+    clickToday(data) {
+      console.log(3)
+      console.log(data); //跳到了本月
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.wh_jiantou1 {
+  display: none !important;
+}
+#home_one {
+  .students-list {
+    .wh_container {
+      .wh_content_all {
+        .wh_top_changge {
+          li {
+            .wh_jiantou1 {
+              display: none !important;
+            }
+            .wh_jiantou2 {
+              display: none;
+            }
+          }
+        }
+      }
+    }
+    .icon-list {
+      line-height: 333px;
+      i {
+        font-size: 24px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
--- src/pages/home/home_one1.vue
+++ src/pages/home/home_one1.vue
@@ -1,10 +1,46 @@
 <template>
   <div id="home_one1">
-    <ele-calendar
-          :render-content="renderContent"
-          :data="datedef"
-          :prop="prop"
-    ></ele-calendar>
+    <el-row>
+      <el-col :xs="2" :sm="2" :md="2" :lg="2" :xl="3">
+        <div class="students-list">
+          <i class="el-icon-arrow-left" @click="leftClick"></i>
+        </div>
+      </el-col>
+      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+        <div class="students-list">
+          <Calendar
+            v-on:choseDay="clickDay"
+            v-on:changeMonth="changeDate"
+            :sundayStart="true"
+            :markDate='checkList'
+            :textTop="['日', '一', '二', '三', '四', '五', '六']"
+          ></Calendar>
+        </div>
+      </el-col>
+      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+        <div class="students-list">
+          <ele-calendar
+                :render-content="renderContent"
+                :data="datedef"
+                :prop="prop"
+          ></ele-calendar>
+        </div>
+      </el-col>
+      <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+        <div class="students-list">
+          <ele-calendar
+                :render-content="renderContent"
+                :data="datedef"
+                :prop="prop"
+          ></ele-calendar>
+        </div>
+      </el-col>
+      <el-col :xs="2" :sm="2" :md="2" :lg="2" :xl="3">
+        <div class="students-list">
+          <i class="el-icon-arrow-right" @click="rightClick"></i>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -15,8 +51,8 @@ export default {
   data () {
     return {
       datedef:[
-        {"date":"2019-10-26","content":null,"cid":null},
-        {"date":"2019-10-30","content":null,"cid":null},
+        {date:"2019-10-26"},
+        {date:"2019-10-30"},
       ],
       prop:'date' //对应日期字段名
     }
@@ -30,14 +66,16 @@ export default {
   },
   methods: {
     renderContent(h,parmas) { //设置lunarcalendar=true,parmas返回值包含农历..//  parmas返回当前日期和传入data对应内容
+    console.log(h)
       const loop = data =>{
         return (
-          data.defvalue.value ? (<div><div class="current" style="background: #1989FA;">{data.defvalue.text}</div>
-          </div>) : <div>{data.defvalue.text}</div>
+          data.defvalue.value ? (
+            <div><div style="background: #00c3d0;color:#fff;border-radius: 50%;">{data.defvalue.text}</div></div>
+          ) : <div>{data.defvalue.text}</div>
         )
       }
       return (
-      <div  style="min-height:60px;">
+      <div style="width:40px;height:40px;border-radius: 50%;text-align:center;line-height:40px;border:1px solid #eee;color:#333;margin:10px;">
         {loop(parmas)}
       </div>
       );
@@ -46,11 +84,23 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-.is-selected {
-  color: #1989FA;
-}
-.current {
-  background: #1989FA;
+<style lang="less">
+#home_one1 {
+  .el-date-table-calendar {
+    tr {
+      th {
+        width:40px !important;
+        height:40px !important;
+        line-height: 40px;
+        text-align: center;
+      }
+    }
+    .el-date-table-calendar__row {
+      td {
+        text-align: center;
+        margin: 0 auto;
+      }
+    }
+  }
 }
 </style>
